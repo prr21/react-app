1d73c5e17a94d2d81f9b8901a1723dfb3f2e477e
prr <Mr.gustav009@gmail.com>
2019-09-08 23:02:14 +0300

    Некоторые изменения

--- src/components/app-search.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const AppSearch = () => {
-	return <input placeholder="search" />
-}
-
-export default AppSearch
--- src/index.js
+++ src/index.js
@@ -1,25 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom';
 
-import AppHeader from './components/app-header'
-import AppSearch from './components/app-search'
-import TodoList from './components/todo-list'
-
-const App = () => {
-
-	const todoData = [
-		{label: "Get coffee", important: false, id: '1'},
-		{label: "Build App", important: true, id: '2'},
-		{label: "Get Rest", important: false, id: '3'}
-	]
-
-	return (
-		<div>
-			<AppHeader />
-			<AppSearch />
-			<TodoList todos={todoData} />
-		</div>
-	)
-}
+import App from './components/app'
 
 ReactDom.render(<App />, document.getElementById('root'));
